a7d047f4b345c8fc3b52c34e973482d265b000e1
Johana Fernandez <najofe5@gmail.com>
2025-03-19 11:50:20 +0100

    feat:Configuracion estado global contador

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch, useSelector } from 'react-redux';
+
+import counterReducer from '../store/counter/counterSlice';
 
 export const makeStore = () => {
   return configureStore({
-    reducer: {},
+    reducer: {
+      counter : counterReducer,
+
+    },
   })
 }
 
@@ -10,4 +16,8 @@ export const makeStore = () => {
 export type AppStore = ReturnType<typeof makeStore>
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore['getState']>
-export type AppDispatch = AppStore['dispatch']
+export type AppDispatch = AppStore['dispatch'];
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
